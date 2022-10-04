ad20684333d43aa47c44ffa6f9c152e0e1296b10
Daniel Mercado <damet.2401@gmail.com>
2022-10-03 22:22:52 -0500

    init and function databes

--- rpm/database.py
+++ rpm/database.py
@@ -12,10 +12,16 @@ DEFAULT_DB_FILE_PATH = Path.home().joinpath(
     "." + Path.home().stem + "_rpm.json"
 )
 
-def get_database_path(db_path: Path) -> int:
-    """Create the repo database."""
+def get_database_path(config_file: Path) -> Path:
+    """Return the current path to the rpm database."""
+    config_parser = configparser.ConfigParser()
+    config_parser.read(config_file)
+    return Path(config_parser["General"]["database"])
+
+def init_database(db_path: Path) -> int:
+    """Create the rpm database."""
     try:
-        db_path.write_text("[]")
+        db_path.write_text("[]")  # Empty to-do list
         return SUCCESS
     except OSError:
         return DB_WRITE_ERROR
